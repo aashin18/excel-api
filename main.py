d919556774bb2dddb328d5c9cec9ab8280e6f6b8
Aashin George <agn2000@hw.ac.uk>
2025-08-07 10:25:36 +0400

    Fix: Bind to 0.0.0.0 and dynamic port

--- main.py
+++ main.py
@@ -261,6 +261,8 @@ def check_address():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 1000))
+    app.run(host='0.0.0.0', port=port)
 
 
