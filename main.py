d9d29d3f4c3b881a3d70e5de8ab3dee012703f4b
Aashin George <agn2000@hw.ac.uk>
2025-08-07 10:37:48 +0400

    Fix: Bind to 0.0.0.0 and dynamic port for render

--- main.py
+++ main.py
@@ -262,7 +262,8 @@ def check_address():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get('PORT', 1000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
 
+ 
